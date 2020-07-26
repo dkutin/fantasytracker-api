1b8c6d376e76f86600d80cff5e85669a2d6e255f
Dmitry Kutin <dmitrykutin@outlook.com>
2020-07-26 12:11:39 -0400

    Add openapi 3.0.0

--- src/api/player/v1/player.controller.js
+++ src/api/player/v1/player.controller.js
@@ -15,23 +15,15 @@ exports.list_all_players = function(req, res) {
 };
 exports.add_player = function(req, res) {
     var player = new Player(req.body);
-
-    if (!player.full_name || !player.image || !player.number || !player.playerId || !player.position || !player.team){
-        res.status(400).json({
-            error: true,
-            message: 'Please provide valid player fields'
-        });
-    } else {
-        Player.add(player, function(err, players) {
-            if (err)
-                res.json(err);
-            else
-                res.status(201).json({
-                    status : 'Successfully added', 
-                    response : players
-                });
-        });
-    }
+    Player.add(player, function(err, players) {
+        if (err)
+            res.json(err);
+        else
+            res.status(201).json({
+                status : 'Successfully added', 
+                response : players
+            });
+    });
 };
 exports.get_player_by_id = function(req, res) {
     Player.getPlayerById(req.params.playerId, function(err, player) {
